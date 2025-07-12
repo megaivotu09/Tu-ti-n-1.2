29eb75ce28bfa1b3c4637702fb12187da1eb3ec6
megaivotu09 <xenosvn123@gmail.com>
2025-07-12 10:11:32 +0700

    Update TuTienPlugin.java

--- src/main/java/vn/yourname/tutien/TuTienPlugin.java
+++ src/main/java/vn/yourname/tutien/TuTienPlugin.java
@@ -8,39 +8,32 @@ import vn.yourname.tutien.task.*;
 
 public class TuTienPlugin extends JavaPlugin {
     private static TuTienPlugin instance;
-    
-    private PlayerManager playerManager;
-    private MeditationManager meditationManager;
-    private SwordFlyManager swordFlyManager;
-    private AttributeManager attributeManager;
-    private BossBarManager bossBarManager;
-    private ThienKiepManager thienKiepManager;
-    private CombatManager combatManager;
-    private InjuryManager injuryManager;
-    private SoulManager soulManager;
-    
+
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
         ConfigManager.loadConfig(this);
+
+        // Initialize Managers
+        PlayerManager playerManager = new PlayerManager();
+        MeditationManager meditationManager = new MeditationManager();
+        SwordFlyManager swordFlyManager = new SwordFlyManager();
+        ThienKiepManager thienKiepManager = new ThienKiepManager();
+        CombatManager combatManager = new CombatManager();
+        InjuryManager injuryManager = new InjuryManager();
+        BossBarManager bossBarManager = new BossBarManager(playerManager);
         
-        // Initialize Managers in correct order
-        this.playerManager = new PlayerManager();
-        this.meditationManager = new MeditationManager();
-        this.swordFlyManager = new SwordFlyManager();
-        this.thienKiepManager = new ThienKiepManager();
-        this.combatManager = new CombatManager();
-        this.injuryManager = new InjuryManager();
-        this.bossBarManager = new BossBarManager(playerManager);
-        this.attributeManager = new AttributeManager();
-        this.soulManager = new SoulManager(this, playerManager, attributeManager, bossBarManager);
+        // Xử lý dependency vòng tròn giữa SoulManager và AttributeManager
+        SoulManager soulManager = new SoulManager(this, playerManager, bossBarManager);
+        AttributeManager attributeManager = new AttributeManager(soulManager);
+        soulManager.setAttributeManager(attributeManager);
 
         // Register Commands
         NgukiemCommand ngukiemCommand = new NgukiemCommand(playerManager, swordFlyManager);
         getCommand("tuvi").setExecutor(new TuViCommand(playerManager));
         getCommand("dotpha").setExecutor(new DotPhaCommand(this, playerManager, bossBarManager, attributeManager, thienKiepManager));
-        getCommand("tuluyen").setExecutor(new TuLuyenCommand(meditationManager));
+        getCommand("tuluyen").setExecutor(new TuLuyenCommand(meditationManager, soulManager));
         getCommand("ngukiem").setExecutor(ngukiemCommand);
         getCommand("anki").setExecutor(new AnKiCommand(playerManager));
         getCommand("xoaan").setExecutor(new XoaAnCommand(playerManager));
@@ -58,26 +51,26 @@ public class TuTienPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new CoreItemListener(this), this);
         getServer().getPluginManager().registerEvents(new SuppressionListener(playerManager, soulManager), this);
         getServer().getPluginManager().registerEvents(new SwordFlyListener(swordFlyManager, ngukiemCommand), this);
-        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this, playerManager, attributeManager, bossBarManager, soulManager), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this, playerManager, bossBarManager, soulManager, attributeManager), this);
         getServer().getPluginManager().registerEvents(new InjuryListener(this, combatManager, injuryManager), this);
-        getServer().getPluginManager().registerEvents(new AmuletListener(this), this);
-        
+        getServer().getPluginManager().registerEvents(new AmuletListener(this, soulManager, playerManager, attributeManager), this);
+
         // Start Tasks
         new ConcealmentListener(this, playerManager, bossBarManager).runTaskTimer(this, 0L, 20L);
         new CultivationTask(playerManager, meditationManager, bossBarManager).runTaskTimer(this, 0L, 20L);
         new ParticleTask(meditationManager, playerManager).runTaskTimer(this, 0L, 4L);
-        new SoulDrainTask(soulManager).runTaskTimer(this, 1200L, 1200L);
+        new SoulDrainTask(soulManager, playerManager).runTaskTimer(this, 1200L, 1200L);
         new SwordFlyTask(playerManager, swordFlyManager, ngukiemCommand).runTaskTimer(this, 0L, 1L);
         new TrackingTask(playerManager).runTaskTimer(this, 0L, 20L);
-        
+
         getLogger().info("TuTienPlugin da duoc bat!");
     }
-    
+
     @Override
     public void onDisable() {
         getLogger().info("TuTienPlugin da duoc tat!");
     }
-    
+
     public static TuTienPlugin getInstance() {
         return instance;
     }
